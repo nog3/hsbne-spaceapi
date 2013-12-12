4c58fcdcbf2f9274056d44ca2e9f11446f720375
Brendan Halliday <nog3@nog3.net>
2013-12-11 21:30:49 -0500

    Added JSON response and stringify.

--- server.js
+++ server.js
@@ -4,35 +4,37 @@ levelup = require("levelup"),
 port = process.env.OPENSHIFT_NODEJS_PORT || 8080,
 ip = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
     
-var spacejson = [{
-    "api": "0.12",
-    "space": "HSBNE",
-    "logo": "http://hsbne.org/assets/img/headerlogo.png",
-    "icon": {
-        "open": "http://hsbne.org/assets/img/headerlogo.png",
-        "closed": "http://hsbne.org/assets/img/headerlogo.png"
+var spaceInfo = {
+    api: "0.12",
+    space: "HSBNE",
+    logo: "http://hsbne.org/assets/img/headerlogo.png",
+    icon: {
+        open: "http://hsbne.org/assets/img/headerlogo.png",
+        closed: "http://hsbne.org/assets/img/headerlogo.png"
     },
-    "url": "http://hsbne.org/",
-    "address": "217 Macarthur Avenue, Eagle Farm QLD 4009",
-    "contact": {
-        "email": "contact@hsbne.org",
-        "irc": "irc://freenode/hsbne",
-        "ml": "hackerspace_brisbane@googlegroups.com",
-        "twitter": "@HSBNE"
+    url: "http://hsbne.org/",
+    address: "217 Macarthur Avenue, Eagle Farm QLD 4009",
+    contact: {
+        email: "contact@hsbne.org",
+        irc: "irc://freenode/hsbne",
+        ml: "hackerspace_brisbane@googlegroups.com",
+        twitter: "@HSBNE"
     },
     
-    "lat": -27.45493,
-    "lon": 153.050181,
-    "open": false,
-    "status": "closed",
-    "lastchange": 1386524299
-}];
+    lat: -27.45493,
+    lon: 153.050181,
+    open: false,
+    status: "closed",
+    lastchange: 1386524299
+};
+
+var spaceJson = JSON.stringify(spaceInfo);
 
 http.createServer(function(request,response){  
     var path = url.parse(request.url).pathname;  
     if (path === "/json") {
         response.writeHead(200, { "Content-Type": "application/json" });
-        response.write(spacejson);
+        response.write(spaceJson);
         response.end();
     }
     else {
